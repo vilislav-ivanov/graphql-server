3a5efc38bbf1cdaa5d1b7442f2e4a7b2b5e9546d
vilislav-ivanov <vilislav.ivanov.ivanov@gmail.com>
2021-06-18 21:22:19 +0300

    hashing pass and saving user entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,22 @@
+import { Field, ID, ObjectType } from 'type-graphql';
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
 
+@ObjectType()
 @Entity()
 export class User extends BaseEntity {
+  @Field((type) => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field({ description: 'some description for firstName prop' })
   @Column()
   firstName: string;
 
+  @Field()
   @Column()
   lastName: string;
 
+  @Field()
   @Column('text', { unique: true })
   email: string;
 
--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,9 +1,44 @@
-import { Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  FieldResolver,
+  Mutation,
+  Query,
+  Resolver,
+  Root,
+} from 'type-graphql';
+import bcrypt from 'bcrypt';
 
-@Resolver()
+import { User } from '../../entity/User';
+
+@Resolver(() => User)
 export class Register {
   @Query(() => String)
   async hello() {
-    return 'Hello World';
+    return 'Hello World!';
+  }
+
+  @FieldResolver(() => String)
+  name(@Root() parent: User) {
+    return `${parent.firstName} ${parent.lastName}`;
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg('firstName') firstName: string,
+    @Arg('lastName') lastName: string,
+    @Arg('email') email: string,
+    @Arg('password') password: string
+  ): Promise<User> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      email,
+      firstName,
+      lastName,
+      password: hashedPass,
+    }).save();
+
+    return user;
   }
 }
