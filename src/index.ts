54890587ec4ae24a94636d7638b44623fc133437
vilislav-ivanov <vilislav.ivanov.ivanov@gmail.com>
2021-06-19 10:30:47 +0300

    set up express-session with redis

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { createConnection } from 'typeorm';
+import session from 'express-session';
+import Redis from 'ioredis';
+import connectRedis from 'connect-redis';
 
+import { redisPass } from '../config';
 import { Register } from './modules/user/Register';
 
 const main = async () => {
@@ -14,6 +18,24 @@ const main = async () => {
   await createConnection();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redis = new Redis({ password: redisPass });
+
+  app.use(
+    session({
+      store: new RedisStore({ client: redis }),
+      secret: 'some random secret ikdfgjkfdgkfdgijkerijoeirjoeoirj',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
+
   apolloServer.applyMiddleware({ app });
   app.listen(4000, () => {
     console.log('app up and running at http://localhost:4000/graphql');
